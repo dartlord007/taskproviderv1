01abdcf9f7e020a151d5715cd84c768b93de1e76
kolade <kolademodupe007@gmail.com>
2023-12-25 10:45:18 +0100

    delete functionality added

--- lib/screens/taskScreens.dart
+++ lib/screens/taskScreens.dart
@@ -71,8 +71,55 @@ class TaskWidget extends StatelessWidget {
         "${task.date}, ${task.time}",
         style: const TextStyle(color: textBlue),
       ),
-      trailing: IconButton(onPressed: (){return deleteTodo(context,task );}, icon: const Icon(Icons.delete),),
+      trailing: IconButton(onPressed: ()async {
+        final result = await confirmDialog(context);
+        if (result!=null && result )
+          {return deleteTodo(context,task );}
+        },
+         icon: const Icon(Icons.delete),),
     );
+  }
+
+  Future<dynamic> confirmDialog(BuildContext context) {
+    return showDialog(
+                          context: context,
+                          builder: (BuildContext context)  {                      
+                          return AlertDialog (
+                            backgroundColor: Colors.grey.shade900,
+                            icon: Icon(Icons.info, color: Colors.grey.shade700,),
+                            title: const Text("Are you sure you want to delete this", style: TextStyle(color: Colors.white ),),
+
+                            content: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+
+                              ElevatedButton(onPressed: (){
+                                Navigator.pop(context, true);
+                              }, 
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                              child: const SizedBox(
+                                width: 50,
+                                height: 20,
+                                child: Text("Yes", 
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white ),),
+                              )),
+
+                              ElevatedButton(onPressed: (){
+                                Navigator.pop(context, false);
+                              }, 
+                              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                              child: const SizedBox(
+                                width: 50,
+                                height: 20,
+                                child: Text("No", 
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white ),),
+                              )),
+                            ]),
+
+                          );
+                          });
   }
 }
 
